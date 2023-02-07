318b47ebae5ec07751a8937b02868bb1dced4c8b
Jan Hatapka <jan.hatapka@gmail.com>
2023-02-07 21:58:00 +0100

    template

--- viewer/models.py
+++ viewer/models.py
@@ -16,8 +16,14 @@ class Profile(Model):
     last_name = CharField(max_length=50)
     gender = ForeignKey(Gender, on_delete=DO_NOTHING)
     city = CharField(max_length=50)
+    followers = IntegerField(max_length=50, default=0)
+    following = IntegerField(max_length=50, default=0)
     about = TextField(max_length=1024)
     profile_picture = CharField(max_length=1024, null=True)
+    photo_link1 = CharField(max_length=1024, null=True)
+    photo_link2 = CharField(max_length=1024, null=True)
+    photo_link3 = CharField(max_length=1024, null=True)
+    photo_link4 = CharField(max_length=1024, null=True)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
