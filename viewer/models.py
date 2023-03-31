6ac53c7b0ac0f62891885b69f9f6f1a669e3756a
Jan Hatapka <jan.hatapka@gmail.com>
2023-03-31 17:09:03 +0200

    status and comment models

--- viewer/models.py
+++ viewer/models.py
@@ -31,3 +31,20 @@ class Profile(Model):
     def __str__(self):
         return f'{self.user}'
 
+
+class Status(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.CharField(max_length=2000)
+    date_created = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='liked_statuses')
+    dislikes = models.ManyToManyField(User, related_name='disliked_statuses')
+
+    class Meta:
+        ordering = ['-date_created']
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.ForeignKey(Status, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
+    date_created = models.DateTimeField(auto_now_add=True)
