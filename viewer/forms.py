07cc3051706ff9bcb33978637272d1f60208b764
Jan Hatapka <jan.hatapka@gmail.com>
2023-05-13 11:37:20 +0200

    sign up style

--- viewer/forms.py
+++ viewer/forms.py
@@ -15,6 +15,16 @@ class SignUpForm(UserCreationForm):
         model = get_user_model()
         fields = ('username','email', 'first_name', 'last_name', 'password1', 'password2')
 
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['first_name'].widget.attrs['class'] = 'form-control'
+        self.fields['last_name'].widget.attrs['class'] = 'form-control'
+        self.fields['email'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
+
 
 
 
