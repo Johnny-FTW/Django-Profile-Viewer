54ff64d82699f7b335345ada72089b0dee2100c4
Jan Hatapka <jan.hatapka@gmail.com>
2023-05-30 19:22:08 +0200

    new chat styles

--- chat/urls.py
+++ chat/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('sent_msg/<str:username>/', sent_messages, name='sent_messages'),
     path('rec_msg/<str:username>/', received_messages, name='received_messages'),
     path('notification', chat_notification, name='notification'),
-    path('test/', test, name='test'),
+    path('test/<str:username>/', test, name='test'),
+    path('index2/', index2, name='index2'),
 ]
